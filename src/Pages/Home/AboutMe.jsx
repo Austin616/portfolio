853cb451beea18e9244e6c47d1c5ee616bc58fba
Austin <austintran616@gmail.com>
2024-08-05 20:17:28 -0500

    first commit

---
+++ src/Pages/Home/AboutMe.jsx
@@ -0,0 +1,25 @@
+export default function AboutMe() {
+    return(
+        <section id="AboutMe" className="about--section">
+            <div className ="about--section--img">
+                <img src="./img/about-me.png" alt="About Me"/>
+            </div>
+            <div className="hero--section--content--box about--section--box">
+                <div className="hero--section--content">
+                    <p className ="section--title">
+                        About
+                    </p>
+                    <h1 className="skills-section--heading">
+                        About Me
+                    </h1>
+                    <p className="hero--section--description">
+                        Hello, I am a full-stack developer with a passion for creating beautiful and functional websites. I have experience in both front-end and back-end development. I am proficient in HTML, CSS, JavaScript, React, Node.js, and MongoDB. I am always eager to learn new technologies and improve my skills. I am a team player and enjoy working with others to create amazing products.
+                    </p>
+                    <p className="hero--section--description">
+                    I am excited to bring my skills to your team and help you achieve your goals.
+                    </p>
+                </div>
+            </div>
+        </section>
+    )
+}
---
+++ src/Pages/Home/HeroSection.jsx
@@ -0,0 +1,24 @@
+export default function HeroSection() {
+  return (
+    <section id="heroSectoin" className="hero--section">
+      <div className="hero--section--content--box">
+        <div className="hero--section--content">
+          <p className="section--title">Hey, I'm Austin</p>
+          <h1 className="hero--section--title">
+            <span className="hero--section-title--color"> Full Stack</span>{" "}
+            <br />
+            Developer
+          </h1>
+          <p className="hero--section--description">
+            hero section description.
+            <br /> hero section description after line break.
+          </p>
+        </div>
+        <button className="btn btn-primary">Get in Touch</button>
+      </div>
+      <div className="hero--section--img">
+        <img src="./img/hero_img.png" alt="hero section img" />
+      </div>
+    </section>
+  );
+}
